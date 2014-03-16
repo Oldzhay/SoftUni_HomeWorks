4ffa2ff8a7cf78600ab5b5047b2a8d4d717f0111
Oldzhay <oldzhay@saroolu.me>
2014-03-16 20:41:59 +0100

    Homework2, Problem 5 - Unicode Value

---
+++ CSharp-Primitive-Data-Types-And-Variables/105. Unicode_Value/Unicode Value.cs
@@ -0,0 +1,18 @@
+﻿/* ==========================
+ * Problem 5.	Unicode Value
+ * ==========================
+ * Declare a character variable and assign it with the symbol that has Unicode code 72, and then print it.
+ * Hint: first, use the Windows Calculator to find the hexadecimal representation of 72.
+ * The output should be “H”.
+*/
+
+using System;
+
+class Unicode_Value
+{
+    static void Main()
+    {
+        char CharVariable = 'H';
+        Console.WriteLine("The Unicode code {0}, corresponds to char {1}", (int)CharVariable, CharVariable);
+    }
+}
