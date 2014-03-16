34e2605369ed8bcef808cc78c025177e80bd1372
Oldzhay <oldzhay@saroolu.me>
2014-03-16 12:44:49 +0100

    Submitted answers of 11, 12 and 13 - text files
    011. Programming languages
    012. Development environments
    013. Differences between C# and .NET

--- CSharp-Basics-Intro-Programing-Homework/015. Age_After_10_Years/Age_After_10_Years.cs
+++ CSharp-Basics-Intro-Programing-Homework/015. Age_After_10_Years/Age_After_10_Years.cs
@@ -2,7 +2,7 @@
  * Problem 15.	* Age after 10 Years
  * =================================
  * Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
- * Your cah check your results here: http://www.mathcats.com/explore/agecalculator.html
+ * Your can check your results here: http://www.mathcats.com/explore/agecalculator.html
  */
 
 using System;
