cd274f4dcf50a9b8bc92e02c18b177f8b08f6095
Oldzhay <oldzhay@saroolu.me>
2014-03-16 22:51:25 +0100

    Homework 2, problem 8 - Quotes In Strings

---
+++ CSharp-Primitive-Data-Types-And-Variables/108. Quotes_In_Strings/Quotes_In_Strings.cs
@@ -0,0 +1,21 @@
+﻿/* ==============================
+ * Problem 8.	Quotes in Strings
+ * ==============================
+ * Declare two string variables and assign them with following value:
+ * The "use" of quotations causes difficulties.
+ * Do the above in two different ways: with and without using quoted strings.
+ * Print the variables to ensure that their value was correctly defined.
+ */
+using System;
+
+class Quotes_In_Strings
+{
+    static void Main()
+    {
+        string StringVariableWOQuotetString = "The \"use\" of quotations causes difficulties.";
+        string StringVariableWithQuotedString = @"The ""use"" of quotations causes difficulties.";
+
+        Console.WriteLine(StringVariableWOQuotetString);
+        Console.WriteLine(StringVariableWithQuotedString);
+    }
+}
