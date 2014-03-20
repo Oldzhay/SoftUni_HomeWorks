e52e70e64eb177914d71c20e9ea3cd891a95b252
Oldzhay <oldzhay@saroolu.me>
2014-03-20 12:48:32 +0100

    Homework 2, problem 11
    Employee Data

---
+++ CSharp-Primitive-Data-Types-And-Variables/111. Employee_Data/Employee_Data.cs
@@ -0,0 +1,36 @@
+﻿/* ==========================
+ * Problem 11.	Employee Data
+ * ==========================
+ * A marketing company wants to keep record of its employees.
+ * Each record would have the following characteristics:
+ * •	First name
+ * •	Last name
+ * •	Age (0...100)
+ * •	Gender (m or f)
+ * •	Personal ID number (e.g. 8306112507)
+ * •	Unique employee number (27560000…27569999)
+ * Declare the variables needed to keep the information for a single employee using appropriate primitive data types.
+ * Use descriptive names.
+ * Print the data at the console.
+ */
+
+using System;
+class Employee_Data
+{
+    static void Main()
+    {
+        string EmployeeFirtsName = "Peter, A.k.a Pesho";
+        string EmployeeLastName = "Smith";
+        byte EmployeeAge = 33;
+        char EmployeeGender = 'M';
+        long EmployeeIDNumber = 8306112507;
+        short EmployeeUniqueNumber = 12345;
+
+        Console.WriteLine("Exployee Firts Name: " + EmployeeFirtsName);
+        Console.WriteLine("Employee Last Name: " + EmployeeLastName);
+        Console.WriteLine("Employee Age: " + EmployeeAge);
+        Console.WriteLine("Employee Gender: " + EmployeeGender);
+        Console.WriteLine("Employee Id Number: " + EmployeeIDNumber);
+        Console.WriteLine("Employee Unique Number: " + 275 + EmployeeUniqueNumber);
+    }
+}
