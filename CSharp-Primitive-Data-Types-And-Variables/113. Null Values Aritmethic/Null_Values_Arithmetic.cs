7b8141b0223a000f8f614f40cf37961f491a3a62
Oldzhay <oldzhay@saroolu.me>
2014-03-25 21:40:15 +0100

    Homework 02, problem 13
    Null Values Arithmetic

---
+++ CSharp-Primitive-Data-Types-And-Variables/113. Null Values Aritmethic/Null_Values_Arithmetic.cs
@@ -0,0 +1,22 @@
+﻿/* ===================================
+ * Problem 13.	Null Values Arithmetic
+ * ===================================
+ * Create a program that assigns null values to an integer and to a double variable.
+ * Try to print these variables at the console.
+ * Try to add some number or the null literal to these variables and print the result.
+ */
+
+using System;
+
+class Null_Values_Arithmetic
+{
+    static void Main()
+    {
+        int? integerVariable = null;
+        double? doubleVariable = null;
+
+        Console.WriteLine("integer Variable: " + integerVariable);
+        Console.WriteLine("double Variable: " + doubleVariable);
+        Console.WriteLine("null plus number: " + (null + 3.14159));
+    }
+}
